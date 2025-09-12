ae63859cbb0f9d81bd6dc66ee09220e74725640f
s <heng.jiang@mail.mcgill.ca>
2025-09-12 17:06:19 +0800

    debug

--- test.py
+++ test.py
@@ -1,3 +1,27 @@
+import numpy as np
+from scipy.ndimage import gaussian_filter1d
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from pathlib import Path
+import re
+import json
+from datetime import datetime
+
+# Configuration
+# MODEL_PATH = '/Users/augustsirius/Desktop/00.Project_DIA-CNN/dia-cnn/20250912/models_medium_20250912_135513/model_epoch_004.pth'
+
+MODEL_PATH = '/wangshuaiyao/jiangheng/dia-cnn/dia-cnn-aistation/model_epoch_004.pth'
+
+print(f'load successful: {MODEL_PATH}')
+
+# DATA_FOLDER = '/Users/augustsirius/Desktop/00.Project_DIA-CNN/dia-cnn/00_test_raw_input/test_scoring_dataset/'
+
+DATA_FOLDER = '/wangshuaiyao/dia-bert-timstof/00.TimsTOF_Rust/02.rust_for_rsm/output_new'\
+
+print(f'load successful: {DATA_FOLDER}')
+
+
 import torch
 import sys
 import platform
